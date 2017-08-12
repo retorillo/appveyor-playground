e6fd7c304a20b7f275c6126d4968c4da37b34222
Retorillo <retorillo@outlook.com>
2017-08-12 19:11:00 +0900

    Use build version as zip name

--- pack.bat
+++ pack.bat
@@ -1,21 +1,24 @@
 @echo off
-if "%OutDir%"=="" exit /b 1
-if "%ProjectName%"=="" exit /b 1
-if "%PlatformTarget%"=="" exit /b 1
+if not defined OutDir exit /b 1
+if not defined ProjectName exit /b 1
+if not defined PlatformTarget exit /b 1
+if not defined BuildVersion echo warning: BuildVersion is not defined >&2
 
 setlocal enabledelayedexpansion
 
-set ZipPath=%ProjectName%_%PlatformTarget%.zip
+set ZipName=%ProjectName%_
+if defined BuildVersion set ZipName=!ZipName!%BuildVersion%_
+set ZipName=!ZipName!%PlatformTarget%.zip
 
 cd /D "%OutDir%" 
 
 if errorlevel 1 exit /b 1
 
-del "!ZipPath!" > NUL
+del "!ZipName!" > NUL
 
-7z a "!ZipPath!" "*.exe" && ^
-7z l "!ZipPath!" && ^
-7z h -scrcSHA256 "!ZipPath!"
+7z a "!ZipName!" "*.exe" && ^
+7z l "!ZipName!" && ^
+7z h -scrcSHA256 "!ZipName!" "*.exe"
 
 if errorlevel 1 exit /b 1
 
